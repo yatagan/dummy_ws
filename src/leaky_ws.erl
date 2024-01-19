5b3c96b3bc7c497f554e26f532afa0ef89ebc87e
Bohdan Korniienko <b.korniienko@betinvest.com>
2024-01-19 12:12:48 +0200

    wait_messages_proceeded

--- src/leaky_ws.erl
+++ src/leaky_ws.erl
@@ -8,6 +8,8 @@
 -export([websocket_info/2]).
 -export([get_wss/0, send_heap_bins/0, send_heap_bins/1, process_infos/0, gcs/0]).
 
+-record(state, {session_info}).
+
 init(Req, Opts) ->
 	{cowboy_websocket, Req, Opts}.
 
@@ -15,16 +17,17 @@ terminate(Reason, _Req, _State) ->
 	lager:warning("WS terminated: ~p", [Reason]).
 	
 websocket_init(State) ->
-	{[{set_options, #{idle_timeout => infinity}}], State}.
+	{[{set_options, #{idle_timeout => infinity}}], #state{}}.
 
 websocket_handle(_Data, State) ->
 	{[], State}.
 
-websocket_info({reply, _Reply, _SessionInfo}, State) ->
-    lager:warning("xz"),
+websocket_info({reply, Reply, SessionInfo}, State) ->
+	Args = <<"{\"request\":\"", Reply/binary, "\",\"response\":\"", Reply/binary,"\"}">>,
+    lager:info("xz ~p", [Args]),
 	% lager:log(warning, [], "xz"),
 	% io:format("xz"),
-    {ok, State};
+    {ok, State#state{session_info = SessionInfo}};
 websocket_info(_Info, State) ->
 	{[], State}.
 
@@ -42,9 +45,39 @@ send_heap_bins() ->
 	send_heap_bins(100000).
 send_heap_bins(N) ->
 	WSs = get_wss(),
-	lists:foreach(fun(Pid) ->
-		[Pid ! {reply, crypto:strong_rand_bytes(64),  #{}} || _ <- lists:seq(1, N)] end,
-		WSs).
+	lists:foreach(fun(Pid) -> send_heap_binaries(Pid, N) end, WSs).
+
+send_heap_binaries(Pid, N) ->
+	Chunks = n_length_chunks(lists:seq(1, N), 10000),
+	lists:foreach(fun(Chunk) ->
+		ok = wait_messages_proceeded(Pid),
+		lists:foreach(fun(_) ->
+			Reply = crypto:strong_rand_bytes(64),
+			SessionInfo = maps:from_list([{crypto:strong_rand_bytes(64), crypto:strong_rand_bytes(64)} || _ <- lists:seq(1, 100)]),
+			Pid ! {reply, Reply, SessionInfo}
+		end, 
+		Chunk
+		)
+	end,
+	Chunks
+	).
+
+n_length_chunks([],_) -> [];
+n_length_chunks(List,Len) when Len > length(List) ->
+	[List];
+n_length_chunks(List,Len) ->
+	{Head,Tail} = lists:split(Len,List),
+	[Head | n_length_chunks(Tail,Len)].
+
+wait_messages_proceeded(Pid) ->
+	#{message_queue_len := ML, total_heap_size := HS} = 
+		maps:from_list(process_info(Pid, [message_queue_len, total_heap_size])),
+	if ML =/= 0 -> 
+		lager:error("WS info: mq len=~b, heap size=~b", [ML, HS]), 
+		timer:sleep(ML), 
+		wait_messages_proceeded(Pid); 
+	true -> ok 
+	end.
 
 process_infos() ->
 	WSs = get_wss(),
